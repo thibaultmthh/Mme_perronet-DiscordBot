d388ea2a8af91078610a6989a81a1c6a6bba9ffe
Thibault <thibault.mathian@free.fr>
2020-04-07 14:45:02 +0200

    corrigebalisecodeX

--- async_test.py
+++ async_test.py
@@ -70,7 +70,8 @@ async def corrige_fautes(message):
             pass
         elif mot in ["http","https", "```"]:
             break
-        elif mot[0] == "`"
+        elif mot[0] == "`":
+            break
         else:
             print(mot)
         #    await client.send_message(694926082763259954,str(message.author)+" a fait une faute en écrivant: "+mot)
